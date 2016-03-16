72a7a7172b37ce339f14cf06ff91340130d48540
MikeCroall <mikebcroall@gmail.com>
2016-03-16 00:20:33 +0000

    PlayGame implemented
    The PlayGame method is now functional.
    The default class to run is now GameManager, although there is still no
    main method.
    Also, everything was compiled, hence all the new .class files.

--- src/me/tqvj24/michaelcroall/GameManager.java
+++ src/me/tqvj24/michaelcroall/GameManager.java
@@ -34,7 +34,58 @@ public class GameManager implements GameManagerInterface {
 
     @Override
     public boolean playGame() {
-        //TODO implement method
-        return false;
+        try {
+            Piece winner = Piece.UNSET;
+            boolean moveMade;
+            while (winner == Piece.UNSET){
+
+                moveMade = false;
+                //Red moves first
+                while(!moveMade){
+                    try{
+                        moveMade = board.placePiece(Piece.RED, redPlayer.makeMove(board.getBoardView()));
+                    } catch (PositionAlreadyTakenException e) {
+                        System.out.println("That position is already taken!");
+                    } catch (InvalidPositionException e) {
+                        System.out.println("That is not a valid position!");
+                    } catch (InvalidColourException e) {
+                        System.out.println("Invalid colour given (likely internal error)");
+                    }
+                    //These catches are here, as they can be retried, and do not mean the game is currently unplayable
+                }
+                winner = board.gameWon();
+                //Ensure Red hasn't won before letting Blue take a turn
+                if(winner == Piece.UNSET) {
+                    moveMade = false;
+                    //Blue moves next
+                    while (!moveMade) {
+                        try {
+                            moveMade = board.placePiece(Piece.BLUE, bluePlayer.makeMove(board.getBoardView()));
+                        } catch (PositionAlreadyTakenException e) {
+                            System.out.println("That position is already taken!");
+                        } catch (InvalidPositionException e) {
+                            System.out.println("That is not a valid position!");
+                        } catch (InvalidColourException e) {
+                            System.out.println("Invalid colour given (likely internal error)");
+                        }
+                        //These catches are here, as they can be retried, and do not mean the game is currently unplayable
+                    }
+                    winner = board.gameWon();
+                }
+            }
+            if (winner == Piece.RED){
+                redPlayer.finalGameState(GameState.WON);
+                bluePlayer.finalGameState(GameState.LOST);
+            }else{
+                redPlayer.finalGameState(GameState.LOST);
+                bluePlayer.finalGameState(GameState.WON);
+            }
+        } catch(NoBoardDefinedException noBoardEx){
+            System.out.println("No board has been defined!");
+        } catch(NoValidMovesException noMovesEx){
+            System.out.println("There are no valid moves to make!");
+        }
+
+        return true; // TODO find out what this return is meant to mean
     }
 }
